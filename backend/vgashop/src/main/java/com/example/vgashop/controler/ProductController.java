43b76d30f746d947dd441a52469832ebed8e017c
Trng-Ngyn <trongtd0773@ut.edu.vn>
2026-03-23 20:28:12 +0700

    test API

--- backend/vgashop/src/main/java/com/example/vgashop/controler/ProductController.java
+++ backend/vgashop/src/main/java/com/example/vgashop/controler/ProductController.java
@@ -1,5 +1,15 @@
 package com.example.vgashop.controler;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
 
+    @GetMapping("/test")
+    public String testApi() {
+        return "API hoat dong thanh cong!";
+    }
 }
